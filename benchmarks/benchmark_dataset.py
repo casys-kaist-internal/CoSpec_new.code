6dddec22939b96c75df91f533d3a8e6cf817ad32
Sangjin Choi <sjchoi@casys.kaist.ac.kr>
2025-06-19 18:54:35 +0900

    .

--- benchmarks/benchmark_dataset.py
+++ benchmarks/benchmark_dataset.py
@@ -1519,8 +1519,8 @@ class OpenCodeReasoningDataset(HuggingFaceDataset):
             
             if not is_valid_sequence(prompt_len, 
                                      output_len,
-                                     max_prompt_len=100000,
-                                     max_total_len=100000): 
+                                     max_prompt_len=10000,
+                                     max_total_len=10000): 
                 continue
             
             sampled_requests.append(
