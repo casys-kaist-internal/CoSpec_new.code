d8288d46e79474fd4d3e530fb5117e2290ed7e7a
sjchoi <sjchoi@casys.kaist.ac.kr>
2026-02-02 21:26:54 +0900

    test passing

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -428,7 +428,7 @@ class LLMEngine:
         and the swap CPU cache.
         """
         start = time.time()
-        if envs.COSPEC:
+        if envs.COSPEC and self.speculative_config.is_primary is not None:
             logger.info("Using CoSpec for KV cache allocation")
             if self.speculative_config.is_primary:
                 logger.info("Primary process, determining num of blocks")
@@ -456,7 +456,7 @@ class LLMEngine:
         self.cache_config.num_gpu_blocks = num_gpu_blocks
         self.cache_config.num_cpu_blocks = num_cpu_blocks
 
-        if envs.COSPEC:
+        if envs.COSPEC and self.speculative_config.is_primary is not None:
             logger.info("Lazy initialize KV cache for CoSpec")
         else:
             self.model_executor.initialize_cache(num_gpu_blocks, num_cpu_blocks)
